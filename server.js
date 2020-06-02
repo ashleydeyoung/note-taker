207497883c8153204f1edce25972c1df3bf6b5ef
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-02 09:53:56 -0400

    adjustments to path.join

--- server.js
+++ server.js
@@ -9,25 +9,22 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+    res.sendFile(path.join(__dirname, "db", "db.json"));
   });
 
-// app.post("/notes", function(req, res) {
-//     var newnote = req.input
-//     var newtext = req.textarea
-//     console.log(newnote)
-//     console.log(newtext)
+app.post("/api/notes", function(req, res) {
+  
 
-//     res.json(path.join(__dirname, "./db/db.json"));
-//   });
+    res.json(path.join(__dirname, "./db/db.json"));
+  });
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
