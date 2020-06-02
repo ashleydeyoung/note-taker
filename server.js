8a040a96439b0a64f5637c1bf2c4617113572d7d
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-01 21:10:39 -0400

    notes page linked

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 var express = require("express");
+var path = require("path")
 
 var app = express();
 var PORT = 8080;
 
-app.use(express.static("public", ["index", "notes.html"]));
+app.use(express.static("public"));
 
 app.get("/api/notes", function (req, res) {
     res.send("index");
 });
 
-// app.get('/notes', function (req, res) {
-//     res.send('notes.html')
-//   })
-// app.use(express.static("public"));
-// app.get("/notes", function (req, res) {
-//   res.send("notes.html");
-// });
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
