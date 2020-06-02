efa85c5417bde0f64d83ca3066d1df72796ee304
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-01 21:48:17 -0400

    api/notes code working

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var path = require("path")
 
 var app = express();
 var PORT = 8080;
-
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/api/notes", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
@@ -14,6 +16,18 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+  });
+
+// app.post("/notes", function(req, res) {
+//     var newnote = req.input
+//     var newtext = req.textarea
+//     console.log(newnote)
+//     console.log(newtext)
+
+//     res.json(path.join(__dirname, "./db/db.json"));
+//   });
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
