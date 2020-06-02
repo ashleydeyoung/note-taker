8bf5b973899743f9c204e7ddd43a5f0b3354923d
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-01 21:18:49 -0400

    fixed broken code

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = 8080;
 app.use(express.static("public"));
 
 app.get("/api/notes", function (req, res) {
-    res.send("index");
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get('/notes', (req, res) => {
